bbb39d3156925eca2f34626f95caf8ad43260548
Egor250625 <ivanovegor700@gmail.com>
2025-12-03 23:08:30 +0300

    feat: create MetricsConfig for all metrics,remove ImportMetrics.java

---
+++ src/main/java/com/itmentorcommunityplatform/dataimporter/metrics/MetricsConfig.java
@@ -0,0 +1,57 @@
+package com.itmentorcommunityplatform.dataimporter.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public Counter usersImportSuccessCounter(MeterRegistry registry) {
+        return Counter.builder("data_importer_users_success_total")
+                .description("Total successfully imported users")
+                .register(registry);
+    }
+
+    @Bean
+    public Counter usersImportErrorCounter(MeterRegistry registry) {
+        return Counter.builder("data_importer_users_error_total")
+                .description("Total failed user imports")
+                .register(registry);
+    }
+
+    @Bean
+    public Timer usersImportDurationTimer(MeterRegistry registry) {
+        return Timer.builder("data_importer_users_import_duration_seconds")
+                .description("Time taken to complete users import in seconds")
+                .publishPercentiles(0.5, 0.95, 0.99)
+                .publishPercentileHistogram()
+                .register(registry);
+    }
+
+    @Bean
+    public Counter profilesImportSuccessCounter(MeterRegistry registry) {
+        return Counter.builder("data_importer_profiles_success_total")
+                .description("Total successfully imported profiles")
+                .register(registry);
+    }
+
+    @Bean
+    public Counter profilesImportErrorCounter(MeterRegistry registry) {
+        return Counter.builder("data_importer_profiles_error_total")
+                .description("Total failed profile imports")
+                .register(registry);
+    }
+
+    @Bean
+    public Timer profilesImportDurationTimer(MeterRegistry registry) {
+        return Timer.builder("data_importer_profiles_import_duration_seconds")
+                .description("Time taken to complete profiles import in seconds")
+                .publishPercentiles(0.5, 0.95, 0.99)
+                .publishPercentileHistogram()
+                .register(registry);
+    }
+}
