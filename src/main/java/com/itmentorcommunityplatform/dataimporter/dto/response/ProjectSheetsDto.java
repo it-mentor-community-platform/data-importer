a640d48e9d8b83051aa0ac25c619a17326430ab7
Sergey Starodubcev <starodubcev_2018@list.ru>
2026-01-28 12:35:19 +0200

    fix(data-importer) Внес правки в Модель
    -(Model) В ProjectSheetsDto и ProjectCreatedEvent добавил аннотации в поля для формирования payload сообщения в стиле snake_case

--- src/main/java/com/itmentorcommunityplatform/dataimporter/dto/response/ProjectSheetsDto.java
+++ src/main/java/com/itmentorcommunityplatform/dataimporter/dto/response/ProjectSheetsDto.java
@@ -1,6 +1,7 @@
 package com.itmentorcommunityplatform.dataimporter.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,11 +10,20 @@ import lombok.Data;
 public class ProjectSheetsDto {
 
     private String date;
+
+    @JsonProperty("roadmap_project")
     private String roadmapProject;
+
+    @JsonProperty("programming_language")
     private String programmingLanguage;
+
     private String repositoryName;
+
+    @JsonProperty("github_repository_url")
     private String githubRepositoryUrl;
+
     private String githubUsername;
+
     private String githubUserUrl;
 
 }
