f7ea6e18580ea6f64dda708c948c2c04623944a3
Sabr <sabo98@mail.ru>
2026-04-10 08:46:24 +0500

    fix: добавил проверку на NULL профиля и ссылки профиля

--- src/main/java/com/itmentorcommunityplatform/dataimporter/service/ProjectImportService.java
+++ src/main/java/com/itmentorcommunityplatform/dataimporter/service/ProjectImportService.java
@@ -98,12 +98,16 @@ public class ProjectImportService {
                     continue;
                 }
 
+                String telegramUsername = profile.details() != null
+                        ? getTelegramUsernameFromUrl(profile.details().getTelegramUrl())
+                        : null;
+
                 ProjectUpsertRequestDto requestDto = new ProjectUpsertRequestDto(
                         profile.telegramUserId(),
                         githubRepositoryLink,
                         programmingLanguage,
                         roadmapProject,
-                        getTelegramUsernameFromUrl(profile.details().getTelegramUrl()),
+                        telegramUsername,
                         parseTimestamp(addedTimestamp),
                         PROJECT_SOURCE_TYPE
                 );
@@ -130,6 +134,7 @@ public class ProjectImportService {
     }
 
     private String getTelegramUsernameFromUrl(String url) {
+        if (url == null || url.isBlank()) return null;
         return url.substring(url.lastIndexOf('/'));
     }
 
