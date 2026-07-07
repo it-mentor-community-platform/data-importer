345d08b81cb54029051e4a6b25cdf912c1ac808f
Ilya <ilplenkov@mail.ru>
2026-07-07 14:35:02 +0300

    feat: добавил интеграцию с /api/project/internal/review

--- src/main/java/com/itmentorcommunityplatform/dataimporter/httpclient/ServiceHttpClient.java
+++ src/main/java/com/itmentorcommunityplatform/dataimporter/httpclient/ServiceHttpClient.java
@@ -6,6 +6,7 @@ import com.itmentorcommunityplatform.dataimporter.dto.request.ProfileUpsertReque
 import com.itmentorcommunityplatform.dataimporter.dto.request.ProjectUpsertRequestDto;
 import com.itmentorcommunityplatform.dataimporter.dto.request.UserUpsertRequestDto;
 import com.itmentorcommunityplatform.dataimporter.dto.response.ProfileByGithubResponseDto;
+import com.itmentorcommunityplatform.dataimporter.dto.response.ProjectReviewSheetsDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -118,6 +119,32 @@ public class ServiceHttpClient {
         } catch (Exception ex) {
             log.error("Failed upsert project in ProjectService: authorTelegramUserId={}, roadmapProject={}",
                     requestDto.authorTelegramUserId(), requestDto.roadmapProject());
+        }
+    }
+
+    public void upsertProjectReview(ProjectReviewSheetsDto requestDto) {
+
+        String uri = props.getProjectServiceBaseUrl() + "/api/project/internal/review";
+
+        try {
+            webClient.post()
+                    .uri(uri)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .bodyValue(requestDto)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .block(Duration.ofSeconds(10));
+
+            log.info("Successfully upsert project review in ProjectService: reviewerTelegramId={}, githubRepositoryUrl={}",
+                    requestDto.getReviewerTelegramUserId(), requestDto.getReviewUrl());
+        } catch (WebClientResponseException wcre) {
+            log.error("ProjectService returned error. status={}, body={}, reviewerTelegramId={}, githubRepositoryUrl={}",
+                    wcre.getStatusCode().value(), wcre.getResponseBodyAsString(),
+                    requestDto.getReviewerTelegramUserId(), requestDto.getReviewUrl());
+            throw wcre;
+        } catch (Exception ex) {
+            log.error("Failed upsert project review in ProjectService: reviewerTelegramId={}, githubRepositoryUrl={}",
+                    requestDto.getReviewerTelegramUserId(), requestDto.getReviewUrl());
         }
     }
 
