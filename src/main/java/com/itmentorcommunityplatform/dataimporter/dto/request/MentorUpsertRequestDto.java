fe6a8f9841c77b9960954ff3520d3faaecfa7a13
Ilya <ilplenkov@mail.ru>
2026-08-10 13:56:50 +0300

    refactor: добавил @JsonNaming(LowerCamelCaseStrategy)

--- src/main/java/com/itmentorcommunityplatform/dataimporter/dto/request/MentorUpsertRequestDto.java
+++ src/main/java/com/itmentorcommunityplatform/dataimporter/dto/request/MentorUpsertRequestDto.java
@@ -1,7 +1,11 @@
 package com.itmentorcommunityplatform.dataimporter.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.util.List;
 
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public record MentorUpsertRequestDto(
         Long mentorTelegramUserId,
         String telegramUrl,
